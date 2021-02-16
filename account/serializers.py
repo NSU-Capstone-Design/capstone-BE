db3b14eaaa89ce3f6dd72dac8430ce1df109cb3a
JeoungSulMo <tjdah0853@gmail.com>
2021-02-16 21:54:48 +0900

    djangorestframework-jwt 제거

--- account/serializers.py
+++ account/serializers.py
@@ -1,12 +1,7 @@
 from django.contrib.auth.models import update_last_login
 from rest_framework import serializers
 from account.models import User
-from django.contrib.auth import authenticate, get_user_model
-from rest_framework_jwt.settings import api_settings
-
-
-jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
@@ -19,7 +14,6 @@ class UserCreateSerializer(serializers.Serializer):
 
     # student_id = serializers.CharField(max_length=10)
 
-
     def create(self, validated_data):
         user = User.objects.create(
             email=validated_data['email'],
@@ -30,31 +24,3 @@ class UserCreateSerializer(serializers.Serializer):
 
         user.save()
         return user
-
-
-class UserLoginSerializer(serializers.Serializer):
-    user_id = serializers.CharField(max_length=64)
-    password = serializers.CharField(max_length=128, write_only=True)
-    token = serializers.CharField(max_length=255, read_only=True)
-
-    def validate(self, data):
-        user_id = data.get("user_id", None)
-        password = data.get("password", None)
-        user = authenticate(user_id=user_id, password=password)
-
-        if user is None:
-            return {
-                'user_id': 'None'
-            }
-        try:
-            payload = jwt_payload_handler(user)
-            token = jwt_encode_handler(payload)
-            update_last_login(None, user)
-        except User.DoesNotExist:
-            raise serializers.ValidationError(
-                '해당 계정이 존재하지 않습니다.'
-            )
-        return {
-            'user_id': user.user_id,
-            'token': token
-        }
