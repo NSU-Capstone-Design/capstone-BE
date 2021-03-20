404a939fa312a9dbdae3e49969de177703bbac70
ldk9205 <limdk0910@gmail.com>
2021-03-21 03:55:40 +0900

    jwt connect post

--- group/models.py
+++ group/models.py
@@ -2,19 +2,24 @@ from django.db import models
 from account.models import User
 
 class Group(models.Model):
-    group_name = models.CharField(verbose_name='그룹명', max_length=64, unique=True)
+    group_name = models.CharField(verbose_name='그룹명', max_length=64)
     description = models.TextField(verbose_name='그룹 소개', null=True)
     group_visible = models.BooleanField(verbose_name='공개여부', default=True)
-    group_master = models.OneToOneField(User, verbose_name='가입자', on_delete=models.CASCADE)
+    group_master = models.ForeignKey(User, verbose_name='그룹장', on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.group_name
+        return '그룹명: ' + self.group_name + ', 그룹장: ' + self.group_master.nickname
 
 
 class GroupManage(models.Model):
-    group_id = models.OneToOneField(Group, verbose_name='그룹명', on_delete=models.CASCADE)
+    group_id = models.ForeignKey(Group, verbose_name='그룹명', on_delete=models.CASCADE)
     member = models.ForeignKey(User, verbose_name='가입자', on_delete=models.CASCADE)
     status = models.BooleanField(verbose_name='가입승인여부', default=False)
 
+    def __flag(self):
+        if self.status:
+            return 'O'
+        return 'X'
+
     def __str__(self):
-        return self.group_id
+        return self.group_id.group_name + ', user: ' + self.member.nickname + ', 가입승인여부: ' + self.__flag()
