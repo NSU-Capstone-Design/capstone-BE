838953ddafcd3fe5163f67bdaef2810f3c2df60d
JeoungSulMo <tjdah0853@gmail.com>
2021-04-04 21:37:13 +0900

    save

--- problemInfo/views.py
+++ problemInfo/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import ProblemInfo
 from .serializers import ProblemSerializer
 
-class ProblemPost(generics.ListCreateAPIView):
-    queryset = ProblemInfo.objects.all()
-    serializer_class = ProblemSerializer
