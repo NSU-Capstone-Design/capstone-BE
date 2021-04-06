0816e9d7728d1d85080e1fb89d67cb9b8681cbb5
jwww0510 <jwwww0510@gmail.com>
2021-04-06 17:07:56 +0900

    수정

--- problemInfo/urls.py
+++ problemInfo/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.TestProblem.as_view()),
+    path('', views.ProblemInfoView.as_view()),
 ]
--- problemInfo/views.py
+++ problemInfo/views.py
@@ -1,20 +1,13 @@
-from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .models import ProblemInfo
-from .serializers import ProblemInfoserializer
+from .serializers import ProblemInfoSerializer
 
-
-class ProblemPost(generics.ListCreateAPIView):
-    queryset = ProblemInfo.objects.all()
-    serializer_class = ProblemInfoserializer
-
-
-class TestProblem(APIView):
+class ProblemInfoView(APIView):
     permission_classes = (AllowAny,)
 
     def get(self, request):
-        content = {'message': 'Hello, World!'}
-        return Response(content)
+        serializer = ProblemInfoSerializer(ProblemInfo.objects.all(), many=True)
+        return Response(serializer.data)
